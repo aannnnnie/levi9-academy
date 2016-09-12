b625a940a1c862c434a5c7ba549315537eea835e
aannnnnie <hannahrechana@gmail.com>
2016-09-12 11:45:26 +0300

    Refuctoring
    Change fetchUserDetails function, add followersList to accountsList, add callback function

--- vanilla.js
+++ vanilla.js
@@ -1,9 +1,7 @@
  
 (function() {
     var contentElement = document.getElementById('user-table');
-    var detailElement = document.getElementById('user-detail')
 	var accountsList = [];
-    var followersList = [];
 
     function createAccount(user) {
         var account = document.createElement('DIV');
@@ -32,25 +30,26 @@
         return login;
     }
 
-    function constructDetails(followersList, selectedUserId){
-        var userDetails = document.createElement('DIV')
-        var selectedUser = document.getElementById(selectedUserId)
-        followersList.forEach(function(follower){
+    function constructDetails(sellectedAccount, selectedUserId){
+        var userDetails = document.createElement('DIV');
+        var selectedUser = document.getElementById(selectedUserId);
+
+        sellectedAccount.followersList.forEach(function(follower){
             var folowerLink = document.createElement('A');
             folowerLink.href = follower.url;
             folowerLink.textContent = follower.login;
             folowerLink.classList.add('element');
             userDetails.appendChild(folowerLink);
-        })
+        });
         selectedUser.appendChild(userDetails);        
     }
 
     function toggleDetail(selectedId) {
-        var selectedUser = document.getElementById(selectedId).lastChild;
-        if (selectedUser.classList == 'hidden'){
-            selectedUser.classList.remove('hidden');
+        var selectedUser = document.getElementById(selectedId);
+        if (selectedUser.lastChild.classList == 'hidden'){
+            selectedUser.lastChild.classList.remove('hidden');
         } else {
-            selectedUser.classList.add('hidden');
+            selectedUser.lastChild.classList.add('hidden');
         }            
     }
 
@@ -65,38 +64,38 @@
 
         contentElement.appendChild(tableBody);
         tableBody.addEventListener('click', function(event){
-        	var selectedUser = event.target.parentElement;
             var selectedUserId = event.target.parentElement.id;
             if (selectedUserId){
-                if (document.getElementById(selectedUserId).querySelector('A')) {
+                let selectedUser = accountsList.find(function(account) {return account.id == selectedUserId });
+                if (selectedUser.followersList) {
                     if (selectedUserId && selectedUserId != 'user-table'){
                         toggleDetail(selectedUserId);
                     };       
                 } else {
-                    fetchUserDetails(accountsList[selectedUserId].followers_url, selectedUserId);
+                    fetchUserDetails(selectedUser.followers_url, selectedUserId, constructDetails);
                 };
             };            
         });
     }
 
-    function fetchUserDetails(followersUrl, selectedUserId){
+    function fetchUserDetails(followersUrl, selectedUserId, callback){
         let request = new XMLHttpRequest();
         request.onreadystatechange = () => {
         if (request.readyState == XMLHttpRequest.DONE && request.status == 200) {
+                let sellectedAccount = accountsList.find(function(account){ return account.id == selectedUserId})
                 followersData = JSON.parse(request.responseText);
-                var followersList = followersData.map(function(follower){
+                sellectedAccount.followersList = followersData.map(function(follower){
                     return {
                         login: follower.login,
                         url: follower.html_url
                     };
                 });
-                constructDetails(followersList, selectedUserId); 
+                callback(sellectedAccount, selectedUserId);       
             }
         }
 
         request.open("GET", followersUrl , true);
         request.send();
-    
     }
 
     function init (accountsList) {
